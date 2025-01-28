45d529ae94c94acaf45667f941507e603f72c080
Artem <skickoartem74@gmail.com>
2025-01-28 21:03:50 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const elements = document.getElementsByTagName('input');
+
+for (const element of elements) {
+  const label = document.createElement('label');
+  const inputId = element.id;
+  const inputName = element.name;
+
+  label.className = 'field-label';
+
+  label.setAttribute('for', inputId);
+  label.setAttribute('textContent', inputName);
+  element.appendChild(label);
+  element.setAttribute('placeholder', inputName);
+}
